523516c10276bdee1df2659806a36322a2140695
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2025-04-09 08:43:41 -0700

    update to use go-whosonfirst-spatial/maptile.PointInPolygonCandidateFeaturessFromTile (#3)
    Co-authored-by: thisisaaronland <thisisaaronland@localhost>

--- http/api/piptile.go
+++ http/api/piptile.go
@@ -4,15 +4,11 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
-	"strconv"
 
 	"github.com/aaronland/go-http-sanitize"
-	"github.com/paulmach/orb"
-	"github.com/paulmach/orb/clip"
-	"github.com/paulmach/orb/geojson"
-	"github.com/paulmach/orb/maptile"
-	wof_reader "github.com/whosonfirst/go-whosonfirst-reader"
+	orb_maptile "github.com/paulmach/orb/maptile"
 	spatial_app "github.com/whosonfirst/go-whosonfirst-spatial/application"
+	"github.com/whosonfirst/go-whosonfirst-spatial/maptile"
 	"github.com/whosonfirst/go-whosonfirst-spatial/query"
 )
 
@@ -25,14 +21,6 @@ func PointInPolygonTileHandler(app *spatial_app.SpatialApplication, opts *PointI
 		logger := slog.Default()
 
 		ctx := req.Context()
-
-		intersects_fn, err := query.NewSpatialFunction(ctx, "intersects://")
-
-		if err != nil {
-			logger.Error("Failed to construct spatial fuction (intersects://)", "error", err)
-			xrpcError(rsp, "Bad request", http.StatusBadRequest)
-			return
-		}
 
 		z, err := sanitize.GetInt(req, P_TILE_Z)
 
@@ -58,80 +46,18 @@ func PointInPolygonTileHandler(app *spatial_app.SpatialApplication, opts *PointI
 			return
 		}
 
-		// START OF put me in a function...
+		zm := orb_maptile.Zoom(uint32(z))
+		map_t := orb_maptile.New(uint32(x), uint32(y), zm)
 
-		zm := maptile.Zoom(uint32(z))
-		tl := maptile.New(uint32(x), uint32(y), zm)
+		// Something something something add filters here...
+		spatial_q := &query.SpatialQuery{}
 
-		bounds := tl.Bound()
-		orb_geom := bounds.ToPolygon()
-
-		geom := geojson.NewGeometry(orb_geom)
-
-		intersects_query := &query.SpatialQuery{
-			Geometry: geom,
-		}
-
-		// END OF put me in a function...
-
-		// Something something something this information is not (?) in the parquet files...
-		// intersects_query.IsCurrent = []int64{1}
-
-		intersects_rsp, err := query.ExecuteQuery(ctx, app.SpatialDatabase, intersects_fn, intersects_query)
+		fc, err := maptile.PointInPolygonCandidateFeaturessFromTile(ctx, app.SpatialDatabase, spatial_q, map_t)
 
 		if err != nil {
 			logger.Error("Failed to execute point in polygon query", "error", err)
 			xrpcError(rsp, "Internal server error", http.StatusInternalServerError)
 			return
-		}
-
-		// To do: For each result:
-		// Fetch geojson Feature
-		// Trim/clip geometries to maptile
-		// Return GeoJSON
-
-		fc := geojson.NewFeatureCollection()
-
-		for _, r := range intersects_rsp.Results() {
-
-			id, err := strconv.ParseInt(r.Id(), 10, 64)
-
-			if err != nil {
-				logger.Error("Failed to derive WOF ID", "id", r.Id(), "error", err)
-				xrpcError(rsp, "Internal server error", http.StatusInternalServerError)
-				return
-			}
-
-			body, err := wof_reader.LoadBytes(ctx, app.SpatialDatabase, id)
-
-			if err != nil {
-				logger.Error("Failed to load body for WOF ID", "id", r.Id(), "error", err)
-				xrpcError(rsp, "Internal server error", http.StatusInternalServerError)
-				return
-			}
-
-			f, err := geojson.UnmarshalFeature(body)
-
-			if err != nil {
-				logger.Error("Failed to unmarshal feature for WOF ID", "id", r.Id(), "error", err)
-				xrpcError(rsp, "Internal server error", http.StatusInternalServerError)
-				return
-			}
-
-			// Clipping happens below
-			fc.Append(f)
-		}
-
-		col := make([]orb.Geometry, len(fc.Features))
-
-		for idx, f := range fc.Features {
-			col[idx] = f.Geometry
-		}
-
-		col = clip.Collection(geom.Geometry().Bound(), col)
-
-		for idx, clipped_geom := range col {
-			fc.Features[idx].Geometry = clipped_geom
 		}
 
 		// Generate ATGeo here...
